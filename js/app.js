dc1dfbc9d595333badd74eb543c8e14934c16317
Ruqaya <ruqayaahabib@gmail.com>
2026-08-12 02:40:41 +0300

    Feat: add starting game structure in HTML and add starting JavaScript constants, variables, cached elements

---
+++ js/app.js
@@ -0,0 +1,28 @@
+console.log("Sea Memory Game")
+
+/*-------------------------------- Constants --------------------------------*/
+const choice = ["turtle","fish","octopus","jellyfish"]
+
+/*-------------------------------- Variables --------------------------------*/
+let computerSeq = []
+let playerSeq = []
+let score = 0
+
+/*------------------------ Cached Element References ------------------------*/
+
+const animalEle = document.querySelectorAll(".animal")
+const messageEle = document.querySelector("#message")
+const scoreEle = document.querySelector("#score")
+
+console.log("Animal buttons:", animalEle)
+console.log("Message:", messageEle)
+console.log("Score:", scoreEle)
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
